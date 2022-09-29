13aa7df879db1c7ce78dd4cb5754c0fe7b27a6ae
nghiaseo <nghiaseo1111988@gmail.com>
2022-09-30 00:25:05 +0700

    c

--- src/app/pages/login/sign-in/sign-in.component.ts
+++ src/app/pages/login/sign-in/sign-in.component.ts
@@ -4,19 +4,22 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
-signinForm!:FormGroup
-  constructor() { }
+  signinForm!: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
     this.signinForm = new FormGroup({
-      name:new FormControl('',Validators.required)
-    })
+      name: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      remember: new FormControl(false),
+    });
+  }
+  login() {
+    if (this.signinForm.valid) {
+      console.log('submit')
     }
-    login(){
-      console.log(this.signinForm.controls['name'].touched)
-    }
-
+  }
 }
--- src/app/shared/components/ns-input/ns-input.component.ts
+++ src/app/shared/components/ns-input/ns-input.component.ts
@@ -1,62 +1,88 @@
-import { Component, forwardRef, HostListener, Input, OnInit, ViewChild } from '@angular/core';
-import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  Component,
+  forwardRef,
+  HostListener,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormControl,
+  NgControl,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-ns-input',
   templateUrl: './ns-input.component.html',
   styleUrls: ['./ns-input.component.scss'],
-  providers:[
+  providers: [
     {
-      provide:NG_VALUE_ACCESSOR,
-      useExisting:forwardRef(()=>NsInputComponent),
-      multi:true
-    }
-  ]
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NsInputComponent),
+      multi: true,
+    },
+  ],
 })
-export class NsInputComponent implements OnInit,ControlValueAccessor {
+export class NsInputComponent implements OnInit, ControlValueAccessor {
+  @ViewChild('nsinputM') input!: NgControl;
+  _value: any;
+  @Input() type: string = 'text';
+  @Input() placeholder: string = 'type something';
+  @Input() name: string = '';
+  @Input() icon: string = '';
+  @Input() label: string = 'default label';
+  @Input() value: string = 'default value';
+  @Input() required = false;
+  formSubmit = false;
 
-_value:any
-@Input() type:string = 'text';
-@Input() placeholder:string = 'type something'
-@Input() name:string = ''
-@Input() icon:string = ''
-@Input() label:string = 'default label'
-@Input() value:string = 'default value'
-@Input() required = false
-  constructor() { }
+  constructor() {}
   writeValue(obj: any): void {
-    if(obj !== undefined){
-      this.nsinput = obj
+    if (obj !== undefined) {
+      this.nsinput = obj;
     }
   }
   registerOnChange(fn: any): void {
-   this.propagateChange = fn
+    this.propagateChange = fn;
   }
   registerOnTouched(fn: any): void {
-    this.propagateTouched = fn
+    this.propagateTouched = fn;
   }
   setDisabledState?(isDisabled: boolean): void {
-   // throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');
   }
-  propagateChange = (_: any) => { };
-  propagateTouched = (_: any) => { };
+  propagateChange = (_: any) => {};
+  propagateTouched = (_: any) => {};
 
-  ngOnInit(): void {
-    console.log(this.type)
+  ngOnInit(): void {}
+  get nsinput() {
+    return this._value;
   }
-  get nsinput(){
-    return this._value
+  set nsinput(val: any) {
+    this._value = val;
+    this.propagateChange(this._value);
   }
-  set nsinput(val:any){
-    this._value = val
-    this.propagateChange(this._value)
+  touched(e: any) {
+    this.propagateTouched(e);
   }
-  touched(e:any){
-    this.propagateTouched(e)
+  onChange(e: any) {
+    if (this.type == 'checkbox') this.propagateChange(e.target.checked);
+    else {
+      console.log(e.target);
+    }
+  }
+  ngDoCheck() {
+    if (this.formSubmit) this.input?.control?.markAsTouched();
+    if (this.type == 'text'||'password') {
+      if (this.input?.control?.pristine && !this.formSubmit) {
+        this.formSubmit = true;
+      }
+    }
   }
 
-  @HostListener('keyup')keyupEvent(){
-  //  console.log(this.nsinput.invalid)
+  @HostListener('keyup') keyupEvent() {
+    //  console.log(this.nsinput.invalid)
   }
-
 }
